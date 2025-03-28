c91457d4ea723787d2c8446fd1bcf4aae2ef16a6
kadir zeren <k4dzeren@gmail.com>
2025-03-28 14:24:13 +0300

    short-circuit,switch-case

--- conditions.js
+++ conditions.js
@@ -99,7 +99,7 @@ const cumle = "Bugün günlerden Cumartesi"
 const kucukHarfler = cumle.toLowerCase()
 const buyukHarfler = cumle.toUpperCase()
 console.log(kucukHarfler)
-console.log(buyukHarfler) 
+console.log(buyukHarfler)
 
 //*********************************************
 //* TERNARY
@@ -143,3 +143,74 @@ let maas = 30000
 //? tek durumlu senaryoda ternary'nin bir degiskene deger aktarması işlemi
 maas = calismaSuresi >= 10 ? maas * 1.5 : maas
 console.log("MAAS:", maas)
+
+//*********************************************
+//* SHORT - CIRCUIT
+//*********************************************
+
+let salary = 40000
+let year = 5
+let marriage = true
+
+//! && operatorü (true ise yanındaki ifadeyi calistir.)
+//? Tek condition ve tek durum
+year >= 10 && (salary = salary * 1.5)
+
+//? 2 condition var ve tek durum
+// year >= 10 && marriage === true && (salary = salary * 1.5)
+
+console.log("SALARY:", salary)
+
+//! || operatorü (false ise yanındaki ifadeyi calistir.))
+const sene = 14
+let result = "ahmet"
+
+sene >= 18 || (result = "cocuk")
+console.log(result)
+
+//? falsy bir degerin tespit edilip buna göre bir degiskene baslangic degeri vermek için kullanıma çok uygundur.
+result = ""
+let myVar = result || "noname"
+console.log(myVar)
+
+//? tek durum var ise short-circuit
+//? 2 durum var ise ternary
+//? 3-6 arasi durum var ise if-else
+//? 6 dan fazla ise switch-case
+
+//*********************************************
+//* SWITCH-CASE
+//*********************************************
+
+const s1 = +prompt("1.sayiyi griniz:")
+const islem = prompt("İslemi girinz: +, - , /, *")
+const s2 = Number(prompt("2.sayiyi griniz:"))
+
+//? null,undefined, "", false, NaN, Boolean(0)  => falsy 5 li
+if (isNaN(s1) || isNaN(s2)) {
+  alert("Lütfen gecerli bir sayi girinz:")
+}
+
+if (islem === "/" && s2 === 0) {
+  alert("Sifira bolme hatasi")
+}
+
+let yekun = 0
+switch (islem) {
+  case "+":
+    yekun = s1 + s2
+    break
+  case "-":
+    yekun = s1 - s2
+    break
+  case "/":
+    yekun = s1 / s2
+    break
+  case "*":
+    yekun = s1 * s2
+    break
+  default:
+    alert("Yanlis islem")
+    break
+}
+console.log(`${s1} ${islem} ${s2} = ${yekun.toFixed(3)}`)
